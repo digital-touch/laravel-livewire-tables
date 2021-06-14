2fb84871c2c62395bc8dab53c5baeae6fe829713
Piotr Kucharski <suspendmode@gmail.com>
2021-06-14 11:54:15 +0200

    native select filter

--- resources/views/includes/filters/native-select-filter.blade.php
+++ resources/views/includes/filters/native-select-filter.blade.php
@@ -3,7 +3,6 @@
     label="{{$filter->label()}}"
     :option-label="$filter->optionLabel"
     :option-value="$filter->optionValue"
-    placeholder="Wybierz..."
     :options="$filter->options()"
     wire:model="filters.{{ $filterId }}"
 />
